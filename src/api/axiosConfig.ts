5da11aa55ab1da6c2e86166040ddffa8f85c4ec6
JavierAQR <nonamesteam123@gmail.com>
2025-07-24 23:47:07 -0500

    Actualizando endpoint axios config

--- src/api/axiosConfig.ts
+++ src/api/axiosConfig.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://edutrack-backend-rw6y.onrender.com/api',
+    baseURL: 'https://edutrack-backend-rw6y.onrender.com',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
