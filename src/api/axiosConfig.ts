27a7ca56b2069a924f398e9ba1cabc1fe777f499
JavierAQR <nonamesteam123@gmail.com>
2025-07-24 23:55:56 -0500

    Reemplazando url base

--- src/api/axiosConfig.ts
+++ src/api/axiosConfig.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://edutrack-frontend-xw88.vercel.app',
+    baseURL: 'https://edutrack-backend-rw6y.onrender.com',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
